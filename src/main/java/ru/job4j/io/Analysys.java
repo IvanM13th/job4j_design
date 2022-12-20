0356a8a91a4f1ed3a546e59c84f64ebc03829d46
IvanM13th <bmchek13@gmail.com>
2022-12-20 09:05:57 +0500

    2. Анализ доступности сервера. [#859 #372582]

--- src/main/java/ru/job4j/io/Analysys.java
+++ src/main/java/ru/job4j/io/Analysys.java
@@ -1,33 +1,31 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysys {
 
-    public void unavailable(String source, String target) throws IOException {
+    public void unavailable(String source, String target) {
         boolean down = false;
         String startTime = "";
         String endTime = "";
-        PrintWriter print = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ));
-        BufferedReader in = new BufferedReader(new FileReader(source));
-        for (String line = in.readLine(); line != null; line = in.readLine()) {
-            String[] s = line.split(" ");
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter print = new PrintWriter(target)) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] s = line.split(" ");
 
-            if (down && !"400".equals(s[0]) && !"500".equals(s[0])) {
-                down = false;
-                endTime = s[1];
-                print.write(startTime + ";" + endTime);
+                if (down && !"400".equals(s[0]) && !"500".equals(s[0])) {
+                    down = false;
+                    endTime = s[1];
+                    print.write(String.format("%s;%s\n", startTime, endTime));
+                }
+
+                if (("400".equals(s[0]) || "500".equals(s[0])) && !down) {
+                    down = true;
+                    startTime = s[1];
+                }
             }
-            if (("400".equals(s[0]) || "500".equals(s[0])) && !down) {
-                down = true;
-                startTime = s[1];
-                endTime = startTime;
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -37,3 +35,23 @@ public class Analysys {
     }
 }
 
+/*
+    PrintWriter print = new PrintWriter(
+            new BufferedOutputStream(
+                    new FileOutputStream(target)
+            ));
+    BufferedReader in = new BufferedReader(new FileReader(source));
+        for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] s = line.split(" ");
+
+                if (down && !"400".equals(s[0]) && !"500".equals(s[0])) {
+                down = false;
+                endTime = s[1];
+                print.write(startTime + ";" + endTime);
+                }
+                if (("400".equals(s[0]) || "500".equals(s[0])) && !down) {
+                down = true;
+                startTime = s[1];
+                endTime = startTime;
+                }
+                }*/
