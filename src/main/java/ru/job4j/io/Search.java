765c77d54ae2caf195ba698bf6b750ee52752240
IvanM13th <bmchek13@gmail.com>
2022-12-21 10:02:20 +0500

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,17 +1,16 @@
 package ru.job4j.io;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-       Path start = Paths.get("F:\\download");
-       search(start, path -> path.toFile().getName().endsWith(".mkv")).forEach(System.out::println);
+        validate(args);
+        Path start = Path.of(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(".mkv")).forEach(System.out::println);
 
     }
 
@@ -19,5 +18,14 @@ public class Search {
         SeacrhFiles searcher = new SeacrhFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("No arguments in main method was found");
+        }
+        if (!".mkv".equals(args[1])) {
+            throw new IllegalArgumentException("No extension type was set");
+        }
     }
 }
