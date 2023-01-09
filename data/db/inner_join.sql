0f85eedcf8f73aee42d7524a699e10cc1afbb045
IvanM13th <bmchek13@gmail.com>
2023-01-09 16:21:41 +0500

    1. Inner join [#373617]

---
+++ data/db/inner_join.sql
@@ -0,0 +1,25 @@
+create table book (
+id serial primary key,
+title varchar(255),
+author_id int references authors(id));
+
+insert into authors (author)
+values ('Джо Аберкромби'), ('Брендон Сандерсон'), ('Борис Акунин'), ('Дмитрий Глуховский');
+
+insert into book (title, author_id)
+values ('Кровь и железо', 1), ('Путь королей', 2), ('Левиафан', 3), ('Любовница смерти', 3), ('Сумерки', null);
+
+select b.title, a.author
+from book as b
+join authors as a on b.author_id = a.id;
+
+select count(b.title) as Количество, a.author as Автор
+from book b
+join authors a on b.author_id = a.id
+group by author
+having count(title) > 1;
+
+select title as наименование, author as Автор
+from book
+join authors on book.author_id = authors.id
+where author is not null
