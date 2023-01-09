0817e91edc117bda408e74b9a3874c27ad50ee0f
IvanM13th <114656684+IvanM13th@users.noreply.github.com>
2023-01-09 12:08:46 +0500

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,19 @@
+create table people (
+id serial primary key,
+name varchar(255),
+city text,
+age int);
+
+insert into people (name, city, age)
+values 
+('Ivan', 'Chelyabinsk', 32),
+('Dmitriy', 'Surgut', 28),
+('Anna', 'Tyumen', 25);
+
+update people
+set age = 52
+where name = 'Anna';
+
+delete from people
+where name = 'Dmitriy';
+
