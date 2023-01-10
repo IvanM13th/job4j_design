c31dc8ad21ea60d6238c70a3cb8787aa05697be7
IvanM13th <bmchek13@gmail.com>
2023-01-10 09:11:24 +0500

    0.1. Связи между таблицами [#373615 #379071]

--- data/db/relations.sql
+++ data/db/relations.sql
@@ -1,18 +1,32 @@
 create table clubs (
 id serial primary key,
-name varchar(255),
+name varchar(255)
 );
 
 create table players (
 id serial primary key,
-name varchar(255),
-club_id int references clubs(id));
+name varchar(255)
+club_id int references clubs(id)
+);
 
 create table clubs (
 id serial primary key,
-name varchar(255));
+name varchar(255)
+);
 
 create table career (
 id serial primary key,
-player_id references players(id),
-positio_id references position(id));
+player_id int references players(id)
+positio_id int references position(id)
+);
+
+create table contract (
+id serial primary key,
+contract_number int
+);
+
+create table contracts (
+id serial primary key,
+name varchar(255),
+contract_id int references contract(id)
+);
