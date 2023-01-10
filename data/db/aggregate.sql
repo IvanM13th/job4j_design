e962d12d4d349c7a3adfd73879d9947223f26f1d
IvanM13th <bmchek13@gmail.com>
2023-01-10 09:24:20 +0500

    2. Группировка и агрегатные функции [#373618 #379131]

--- data/db/aggregate.sql
+++ data/db/aggregate.sql
@@ -1,3 +1,29 @@
+insert into devices (name, price)
+values ('samsung tv', 45000),
+        ('iphone', 80000),
+        ('pocophone', 4000),
+        ('xbox', 24000),
+        ('laptop', 38000);
+
+insert into peeople (name)
+values ('Ivan'),
+        ('Sergei'),
+        ('Dmitriy'),
+        ('Anna'),
+        ('Egor');
+
+insert into devices_people (devive_id, people_id)
+values (1,1),
+        (1,3),
+        (1,5),
+        (2,2),
+        (3,3),
+        (3,4),
+        (4,1),
+        (4,5),
+        (5,2),
+        (5,4);
+
 select avg(d.price)
 from devices_people dp
 join devices d on dp.device_id = d.id;
