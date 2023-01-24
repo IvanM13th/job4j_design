676d7738c6bc24a0774628f105fb6ff5a702b279
IvanM13th <bmchek13@gmail.com>
2023-01-24 11:21:26 +0500

    3. Шаблонизатор. [#855]

--- src/test/java/ru/job4j/template/StringGeneratorTest.java
+++ src/test/java/ru/job4j/template/StringGeneratorTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.template;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+@Disabled
 public class StringGeneratorTest {
 
     /**
