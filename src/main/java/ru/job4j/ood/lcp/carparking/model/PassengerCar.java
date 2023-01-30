c426430e0440f54e50ac6994218ddfdc3a0bff51
IvanM13th <bmchek13@gmail.com>
2023-01-30 16:00:54 +0500

    2. Парковка машин [#853 #385148]

--- src/main/java/ru/job4j/ood/lcp/carparking/model/PassengerCar.java
+++ src/main/java/ru/job4j/ood/lcp/carparking/model/PassengerCar.java
@@ -1,9 +1,9 @@
 package ru.job4j.ood.lcp.carparking.model;
 
 public class PassengerCar extends Vehicle {
+    private final int size = 1;
 
     public PassengerCar(int regNumber) {
         super(regNumber);
     }
-
 }
