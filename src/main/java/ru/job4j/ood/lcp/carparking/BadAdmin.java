a0a306a16a4ed143ebb2106bf9eb505dd6cf80ae
IvanM13th <bmchek13@gmail.com>
2023-01-31 12:06:10 +0500

    0. Принцип инверсии зависимостей [#4917]

--- src/main/java/ru/job4j/ood/lcp/carparking/BadAdmin.java
+++ src/main/java/ru/job4j/ood/lcp/carparking/BadAdmin.java
@@ -1,5 +1,6 @@
 package ru.job4j.ood.lcp.carparking;
 
+import ru.job4j.ood.lcp.carparking.model.PassengerCar;
 import ru.job4j.ood.lcp.carparking.model.Vehicle;
 
 import java.util.ArrayList;
@@ -8,15 +9,15 @@ import java.util.List;
 public class BadAdmin {
     private List<Vehicle> pCars = new ArrayList<>();
     private List<Vehicle> trucks = new ArrayList<>();
-    private Vehicle vehicle;
+    private PassengerCar passengerCar;
 
-    public BadAdmin(List<Vehicle> pCars, List<Vehicle> trucks, Vehicle vehicle) {
+    public BadAdmin(List<Vehicle> pCars, List<Vehicle> trucks, PassengerCar passengerCar) {
         this.pCars = pCars;
         this.trucks = trucks;
-        this.vehicle = vehicle;
+        this.passengerCar = passengerCar;
     }
 
-    private boolean add(List<Vehicle> list, Vehicle vehicle) {
+    private boolean add(List<Vehicle> list, PassengerCar passengerCar) {
         return false;
     }
 
